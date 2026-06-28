db4af72595e2352fc07f3b0e881cfe41c20c88fe
Alvaro <alvarohernan97@gmail.com>
2026-06-28 10:47:42 +0200

    Vuelta al origen

--- server_rfid.py
+++ server_rfid.py
@@ -3,7 +3,7 @@
 # Ejecutar: python3 server_rfid.py
 # Requiere: pip install flask cryptography flask-cors
 
-from flask import Flask, request, jsonify, send_file
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 import json, os, time, secrets, hmac
 from cryptography.hazmat.primitives.cmac import CMAC
@@ -183,7 +183,6 @@ def require_session(f):
         return f(*args, **kwargs)
 
     return wrapper
-
 
 # ── Autenticación mutua ───────────────────────────────────────────────────────
 
